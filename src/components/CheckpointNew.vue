01359be29b6a398c8a487107cd9089ce0b6577e7
Eugene Derevyanko <eugenedrvnk@gmail.com>
2019-09-15 20:24:12 +0300

    add checkpoint_new modal

---
+++ src/components/CheckpointNew.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-dialog :value="true" @input="input" width="80vw">
+    <v-card>
+      <div class="checkpoint_new_modal">
+        <v-form v-model="valid">
+          <v-text-field
+            label="Title"
+            v-model="title"
+            :rules="titleRules"
+            color="green"
+            outlined
+            required
+          ></v-text-field>
+          <v-textarea
+            label="Description"
+            v-model="description"
+            auto-grow
+            rows="4"
+            row-height="25"
+            color="green"
+            outlined
+            required
+          ></v-textarea>
+          <v-text-field
+            label="Map Place"
+            v-model="mapPlace"
+            color="green"
+            outlined
+            required
+          ></v-text-field>
+        </v-form>
+      </div>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      valid: false,
+      titleRules: [
+        v => !!v || "Title is required",
+        v => v.length <= 20 || "Name must be less than 20 characters"
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.checkpoint_new_modal {
+  padding: 20px;
+}
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="slider">
+    <slick class="main_slider" ref="slick" :options="slickOptionsMain">
+      <div class="slide">
+        <app-map
+          style="width: 100%; height: 100%"
+          :displayedItemId="displayedItemId"
+          :displayedItemType="displayedItemType"
+        />
+      </div>
+      <div
+        v-for="(item, index) in images"
+        :key="index"
+        class="slide"
+        :style="{background: `url('${item}') no-repeat center center`}"
+      ></div>
+    </slick>
+
+    <slick class="sub_slider" ref="slick" :options="slickOptionsSub">
+      <div class="sub_slide_wrapper">
+        <div
+          class="sub_slide"
+          style="background: url('http://1001idea.info/wp-content/uploads/2018/02/spring-travel.jpg') no-repeat center center"
+        ></div>
+      </div>
+      <div 
+        class="sub_slide_wrapper"
+        v-for="(item, index) in images" :key="index">
+        <div
+          class="sub_slide"
+          :style="{background: `url('${item}') no-repeat center center`}"
+        >{{ index }}</div>
+      </div>
+    </slick>
+  </div>
+</template>
+
+<script>
+import AppMap from "./AppMap";
+import Slick from "vue-slick";
+import "slick-carousel/slick/slick.css";
+
+export default {
+  props: [
+    "displayedItemId",
+    "displayedItemType",
+    "slickOptionsMain",
+    "slickOptionsSub",
+    "images"
+  ],
+  components: {
+    AppMap,
+    Slick
+  }
+};
+</script>
+
+<style lang="scss">
+.slider {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  display: flex;
+  overflow-y: auto;
+  overflow-x: hidden;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.main_slider {
+  width: 100%;
+  height: calc(100% - 120px);
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 0 4px 0px rgba((0), 0, 0, 0.1);
+}
+
+.slick_active {
+}
+
+.sub_slider {
+  height: 100px;
+  overflow: hidden;
+  .sub_slide {
+    transition: 1s;
+    border: 5px solid rgb(221, 237, 214);
+  }
+  .slick-current .sub_slide {
+    transition: 1s;
+    border: 5px solid rgb(138, 210, 116);
+  }
+}
+
+.slide {
+  width: 100%;
+  height: 100%;
+}
+
+.sub_slide_wrapper {
+  width: 100%;
+  height: 100px;
+  .sub_slide {
+    width: calc(100% - 5px);
+    height: 100%;
+  }
+}
+</style>
