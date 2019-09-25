5223b0d78d160b34bed48ef576c603caf29f62b4
eugenedrvnk <eugenedrvnk@gmail.com>
2019-09-25 16:30:21 +0300

    add pixi

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -2,16 +2,31 @@
   <div class="about_wrapper">
     <div class="about">
       <h1>about us page</h1>hello tha'ts some contnt
+      <div class="canvas"></div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import * as PIXI from "pixi.js";
+
+export default {
+  mounted() {
+    const app = new PIXI.Application({width: 1000, height: 1000, transparent: true});
+ 
+    // The application will create a canvas element for you that you
+    // can then insert into the DOM.
+    document.querySelector(".canvas").appendChild(app.view);
+    let container = new PIXI.container();
+    app.stage.addChild(container);
+    let bg = PIXI.Sprite.fromImage()
+ }
+};
 </script>
 
 <style lang="scss" scoped>
 .about {
+  position: relative;
   clip-path: circle(0% at center);
   display: flex;
   flex-direction: column;
@@ -26,5 +41,11 @@ export default {};
 .about_wrapper {
   //   height: 100%;
   width: 100%;
+}
+
+.canvas {
+    position: absolute;
+    width: 100%;
+    height: 100%;
 }
 </style>
